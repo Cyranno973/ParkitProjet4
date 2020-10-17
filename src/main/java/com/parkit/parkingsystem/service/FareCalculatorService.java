e92562ba344f4a4eedc60388e3876ba863699afb
Cyranno973 <gs.gouin@gmail.com>
2020-10-17 16:22:27 +0200

    TODO#2 : Test d'integration OK (TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability)

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,11 +10,19 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+//        int inHour = ticket.getInTime().getHours();
+//        int outHour = ticket.getOutTime().getHours();
+
+        double inHour = ticket.getInTime().getTime();
+        double outHour = ticket.getOutTime().getTime();
+
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+       // int duration = outHour - inHour;
+
+        double duration = outHour - inHour;
+
+        duration = duration/1000d/60d/60d;
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,15 +1,16 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import junit.framework.Assert;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -50,8 +51,33 @@ public class ParkingDataBaseIT {
     @Test
     public void testParkingACar(){
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        int nextParkingSpot =  parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
         parkingService.processIncomingVehicle();
+
+
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+
+        //Etape1 recuperer le ticket du vehicule imatriculer 'ABCDEF'
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+
+        //ETape2 verifier l'existence du ticket
+        Assert.assertNotNull(ticket);
+
+        //ETape3 recuperer le parking spot
+        ParkingSpot parkingSpot = ticket.getParkingSpot();
+
+        //ETape4 verifier son existence
+        Assert.assertNotNull(parkingSpot);
+
+        //ETape5 verfier l'etat de la colonne availaibale est a false
+        Assertions.assertFalse(parkingSpot.isAvailable());
+
+        //ETape5 verfier l'etat de la colonne availaibale est a false
+        Assertions.assertFalse(parkingSpot.isAvailable());
+
+        //ETape6 verfier que la prochaine place disponible a bien été utliser
+        Assertions.assertEquals(nextParkingSpot,parkingSpot.getId());
+
     }
 
     @Test
