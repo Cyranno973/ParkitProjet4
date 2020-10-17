598285a6fa09df8cca2dba266975086003c9119d
Cyranno973 <gs.gouin@gmail.com>
2020-10-17 19:34:41 +0200

    TODO#3 : Test d'integration OK (TODO: check that the fare generated and out time are populated correctly in the database)

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,5 +1,6 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
@@ -14,6 +15,9 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
+
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -81,11 +85,44 @@ public class ParkingDataBaseIT {
     }
 
     @Test
-    public void testParkingLotExit(){
+    public void testParkingLotExit() throws InterruptedException {
         testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        Thread.sleep(5000); // sinon l'entrer et la sortie est faite en quelque millieme de seconde
         parkingService.processExitingVehicle();
+
         //TODO: check that the fare generated and out time are populated correctly in the database
+
+        //Etape on recupere le ticket de la voiture
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+
+        //Etape on verifie l'éxistence
+        Assert.assertNotNull(ticket);
+
+        //Etape on recupere le time in
+         Date timeIn = ticket.getInTime();
+
+        //Etape on verifie l'éxistence
+        Assert.assertNotNull(timeIn);
+
+        //Etape on recupere le time out
+        Date timeOut = ticket.getOutTime();
+
+        //Etape on verifie l'éxistence
+        Assert.assertNotNull(timeOut);
+
+        //Etape on recupere le prix du ticket pour une voiture
+        double price = ticket.getPrice();
+        System.out.println(price);
+
+        // on verifie son existance
+        Assert.assertNotNull(price);
+
+
+        // Etape verfication du pris attendu et du prix obtenu
+        // 5d corespond au 5 sec attendu( Thread.sleep(5000));
+        Assertions.assertEquals(Math.round((5d/3600d)* Fare.CAR_RATE_PER_HOUR),Math.round(ticket.getPrice()));
+
     }
 
 }
